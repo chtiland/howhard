1d31353945a74eb8a412be42bac5b17b497fb062
Fabrice DEJAIGHER <fabrice@chtiland.com>
2012-03-04 23:09:05 +0100

    German translation

---
+++ lang/de/lang.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * German language file for howhard plugin
+ *
+ * @author taggic http://forum.dokuwiki.org/user/6866
+ */
+$lang['howhardtitle'] = 'Komplexit&auml;t';       // Schwierigkeitsgrad, Anforderung
+$lang['level1'] = 'sehr leicht';                  // sehr einfach     - Neuling
+$lang['level2'] = 'leicht';                       // einfach          - Anfänger
+$lang['level3'] = 'mittel';                       // durchschnittlich - Anwender
+$lang['level4'] = 'schwer';                       // hoch             - Fachmann
+$lang['level5'] = 'sehr schwer';                  // sehr hoch        - Meister
+?>
---
+++ lang/de/settings.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * German language file for howhard plugin
+ *
+ * @author taggic http://forum.dokuwiki.org/user/6866
+ */
+
+$lang['confhowhardstyle'] = 'W&auml;hle ein Bild :<br>1 : <img src="lib/plugins/howhard/images/style1/3.png"><br>2 : <img src="lib/plugins/howhard/images/style2/3.png">';
+?>
