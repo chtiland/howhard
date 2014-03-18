289c54c8df08de031fce599d759d44f907943a3f
Rene <wllywlnt@yahoo.com>
2014-03-18 15:40:28 +0100

    translation update

---
+++ lang/nl/lang.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Rene <wllywlnt@yahoo.com>
+ */
+$lang['howhardtitle']          = 'Moeilijkheid';
+$lang['level1']                = 'Erg makkelijk';
+$lang['level2']                = 'Gemakkelijk';
+$lang['level3']                = 'Gemiddeld';
+$lang['level4']                = 'Moeilijk';
+$lang['level5']                = 'Erg Moeilijk';
