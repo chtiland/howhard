d019002d470bab90409a2bdaec1f344f16557f6a
ilker R. Kapac <irifat@gmail.com>
2014-04-30 08:25:05 +0200

    translation update

---
+++ lang/tr/settings.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author ilker R. Kapac <irifat@gmail.com>
+ */
+$lang['confhowhardstyle']      = 'Kullanılacak görseli seçin :<br>1 : <img src="lib/plugins/howhard/images/style1/3.png"><br>2 : <img src="lib/plugins/howhard/images/style2/3.png">';
