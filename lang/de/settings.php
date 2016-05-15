0d1e4cfd8be212e35d75906f6e3210ae6575017f
chtiland <fabrice@MacBook-Pro-de-Fabrice.local>
2016-05-15 15:34:21 +0200

    Parsing error with german translation

---
+++ lang/de/settings.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * German language file for howhard plugin
+ *
+ * @author taggic http://forum.dokuwiki.org/user/6866
+ */
+
+$lang['confhowhardstyle'] = 'W&auml;hle ein Bild :<br>1 : <img src="'.DOKU_BASE.'lib/plugins/howhard/images/style1/3.png"><br>2 : <img src="'.DOKU_BASE.'lib/plugins/howhard/images/style2/3.png"><br><br>3 : <img src="'.DOKU_BASE.'lib/plugins/howhard/images/style3/3.png"><br><br>4 : <img src="'.DOKU_BASE.'lib/plugins/howhard/images/style4/3.png">';
