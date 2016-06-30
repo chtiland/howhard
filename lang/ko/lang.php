4223e7f6a66313d8152921208b3c73414f1196de
Myeongjin <aranet100@gmail.com>
2016-06-30 15:50:32 +0200

    translation update

---
+++ lang/ko/lang.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Myeongjin <aranet100@gmail.com>
+ */
+$lang['howhardtitle']          = '난이도';
+$lang['level1']                = '매우 쉬움';
+$lang['level2']                = '쉬움';
+$lang['level3']                = '보통';
+$lang['level4']                = '어려움';
+$lang['level5']                = '매우 어려움';
