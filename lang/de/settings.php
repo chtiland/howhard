6df15a89cf0c235780da6050d6888a8491c42fac
Chtiland <fabrice@chtiland.com>
2026-01-30 09:36:23 +0100

    Update settings.php
    Wrong keyname

--- lang/de/settings.php
+++ lang/de/settings.php
@@ -6,5 +6,5 @@
  * @author Hella <hella.breitkopf@gmail.com>
  * @author taggic http://forum.dokuwiki.org/user/6866
  */
-$lang['compact']               = 'Kompakte Anzeige (Funktioniert nicht mit Stil 1)';
+$lang['confhowhardcompact']    = 'Kompakte Anzeige (Funktioniert nicht mit Stil 1)';
 $lang['confhowhardstyle']      = 'W&auml;hle ein Bild :<br>1 : <img src="/lib/plugins/howhard/images/style1/3.png"><br>2 : <img src="/lib/plugins/howhard/images/style2/3.png"><br><br>3 : <img src="/lib/plugins/howhard/images/style3/3.png"><br><br>4 : <img src="/lib/plugins/howhard/images/style4/3.png">';
