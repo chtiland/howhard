3ebc9ae9cbdec36a47f72b22f497ba6c78df5a38
Chtiland <fabrice@chtiland.com>
2026-01-30 09:42:10 +0100

    Update settings.php

--- lang/tr/settings.php
+++ lang/tr/settings.php
@@ -6,5 +6,5 @@
  * @author ilker R. Kapac <irifat@gmail.com>
  * @author İlker Kapaç <irifat@gmail.com>
  */
-$lang['compact']               = 'Basit Düzen';
+$lang['confhowhardcompact']               = 'Basit Düzen';
 $lang['confhowhardstyle']      = 'Kullanılacak görseli seçin :<br>1 : <img src="/lib/plugins/howhard/images/style1/3.png"><br>2 : <img src="/lib/plugins/howhard/images/style2/3.png"><br><br>3 : <img src="/lib/plugins/howhard/images/style3/3.png"><br><br>4 : <img src="/lib/plugins/howhard/images/style4/3.png">';
