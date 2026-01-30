ab921de24b8b5453d115cae9028139ff2daf8d23
Chtiland <fabrice@chtiland.com>
2026-01-30 13:15:09 +0100

    Update settings.php

--- lang/en/settings.php
+++ lang/en/settings.php
@@ -5,9 +5,5 @@
  * @author Fabrice DEJAIGHER <fabrice@chtiland.com>
  */
 
-// keys need to match the config setting name
-// $lang['fixme'] = 'FIXME';
-
-
 $lang['confhowhardcompact'] = 'Compact style (not working with style 1)';
 $lang['confhowhardstyle'] = 'Choose image :<br>1 : <img src="'.DOKU_BASE.'lib/plugins/howhard/images/style1/3.png"><br>2 : <img src="'.DOKU_BASE.'lib/plugins/howhard/images/style2/3.png"><br><br>3 : <img src="'.DOKU_BASE.'lib/plugins/howhard/images/style3/3.png"><br><br>4 : <img src="'.DOKU_BASE.'lib/plugins/howhard/images/style4/3.png">';
