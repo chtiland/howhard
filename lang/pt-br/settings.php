47b9a7f30f37acd5b868cec5e1fa530d7c45df31
Chtiland <fabrice@chtiland.com>
2026-01-30 09:41:47 +0100

    Update settings.php

--- lang/pt-br/settings.php
+++ lang/pt-br/settings.php
@@ -5,5 +5,5 @@
  *
  * @author Alexandre Belchior <alexbelchior@gmail.com>
  */
-$lang['compact']               = 'Estilo compacto (aspecto pobre com estilo 1)';
+$lang['confhowhardcompact']               = 'Estilo compacto (aspecto pobre com estilo 1)';
 $lang['confhowhardstyle']      = 'Escolha a imagem :<br>1 : <img src="/lib/plugins/howhard/images/style1/3.png"><br>2 : <img src="/lib/plugins/howhard/images/style2/3.png"><br><br>3 : <img src="/lib/plugins/howhard/images/style3/3.png"><br><br>4 : <img src="/lib/plugins/howhard/images/style4/3.png">';
